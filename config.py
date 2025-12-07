dac087ce1a7526bb0ba2b8b4855a53c9f2c3d52d
DorisEdo <167077232+DorisEdo@users.noreply.github.com>
2025-12-07 18:59:48 +0000

    completed step 17

--- config.py
+++ config.py
@@ -6,7 +6,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY") or "replace-with-real-key-during-production",
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL") or \
-    "sqlite:///" + os.path.join(
-    basedir, "app.db")
+    "sqlite:///" + os.path.join(basedir, "app.db")
     
     
