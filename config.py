b584ab2080f1e26cc024db34b8ee448d4b20da35
DorisEdo <167077232+DorisEdo@users.noreply.github.com>
2025-12-07 21:46:40 +0000

    completed step 18

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ load_dotenv()
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY") or "replace-with-real-key-during-production",
+    SECRET_KEY = os.getenv("SECRET_KEY") or "replace-with-real-key-during-production"
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL") or \
     "sqlite:///" + os.path.join(basedir, "app.db")
     
--- routes.py
+++ routes.py
@@ -1,7 +1,12 @@
-from app import app
-from flask import render_template, request,redirect, url_for,flash
+from app import app, db
+from flask import render_template, request, redirect, url_for, flash
 from datetime import datetime
 from forms import LoginForm
+from models import User, Todo
+from flask_login import current_user, login_user, logout_user, login_required
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from urllib.parse import urlsplit
 
 
 todos = [
@@ -27,24 +32,29 @@ todos = [
 
 
 @app.route("/")
+@login_required
 def index():
     todo_count = len(todos)
     return render_template("index.html", count=todo_count)
 
 
 @app.route("/tasks")
+@login_required
 def all_tasks():
     return render_template("tasks.html", todos=todos)
 
 
 @app.route("/task/<int:task_id>")
+@login_required
 def task(task_id):
     task = todos[task_id - 1]
-    return render_template("task.html", task=task )
+    return render_template("task.html", task=task)
+
 
 @app.route("/edit-task/<int:task_id>", methods=["GET", "POST"])
+@login_required
 def edit_task(task_id):
-    index = task_id - 1 
+    index = task_id - 1
     task = todos[index]
     if request.method == "POST":
         title = request.form.get("title")
@@ -54,34 +64,57 @@ def edit_task(task_id):
         return redirect(url_for("task", task_id=task_id))
     return render_template("task_form.html", task=task)
 
-@app.route("/new-task", methods= ["GET", "POST"])
+
+@app.route("/new-task", methods=["GET", "POST"])
+@login_required
 def create_task():
     if request.method == "POST":
         task_id = todos[-1]["id"] + 1
         title = request.form.get("title")
         description = request.form.get("description")
-        todos.append({
-            "id": task_id,
-            "title":title,
-            "description":description,
-            "created at": datetime.now()
-        })
+        todos.append(
+            {
+                "id": task_id,
+                "title": title,
+                "description": description,
+                "created at": datetime.now(),
+            }
+        )
         return redirect(url_for("task", task_id=task_id))
     return render_template("task_form.html", task=None)
 
+
 @app.route("/dete-task/ <int:task_id>", methods=["POST"])
+@login_required
 def delete_task(task_id):
     global todos
-    todos = [todo for todo in todos if todo["id"] !=task_id]
+    todos = [todo for todo in todos if todo["id"] != task_id]
     return redirect(url_for("all_task"))
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    form = LoginForm()
-    print("login")
-    if form.validate_on_submit():
-        flash(f"login requested for user {form.username.data}, remember_me={form.remember_me.data}")
-        print("form submitted")
+    if current_user.is_authenticated:
         return redirect(url_for("index"))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data)
+        )
+        if user is None or not user.get_password(form.password.data):
+            flash("Invalid username or password")
+            return redirect(url_for("login"))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get("next")
+        # To determine if the URL is absolute or relative,
+        # I parse it with Python's urlsplit() function and then
+        # check if the netloc component is set or not.
+        if not next_page or urlsplit(next_page).netloc != "":
+            next_page = url_for("index")
+        return redirect(next_page)
     return render_template("login.html", form=form)
-    
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
